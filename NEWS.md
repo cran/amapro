10979fceb24b01e64e0caef720b5fc0be26f08dc
Larry Helgason <larry@helgasoft.com>
2022-08-23 06:00:06 +0000

    version 0.1.1

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,7 @@
 # History of amapro package development
 
+## v. 0.1.1
+- fixing documentation notes for CRAN
+
 ## v. 0.1.0
 - initial release, resubmit to CRAN Jul 20, 2022
--- inst/doc/info.R
+++ inst/doc/info.R
@@ -1,3 +1,3 @@
 ## ---- echo = FALSE, message = FALSE-------------------------------------------
-knitr::opts_chunk$set(collapse = T, comment = "#>")
+knitr::opts_chunk$set(collapse = TRUE, comment = "#>")
 
